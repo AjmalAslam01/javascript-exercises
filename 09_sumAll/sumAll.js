e26ebb1e9899b25fd038214d41c4bf4e4915d6ea
AjmalAslam <aslamajmal55@gmail.com>
2025-08-13 01:55:44 +0800

    COMPLETED

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    
+    if(min > max){
+        [min, max] = [max, min];
+    }
 
+    if(!Number.isInteger(min) || !Number.isInteger(max) || (min || max) < 0){
+        return "ERROR";
+    }
+    
+    for(; min <= max; min++){
+        sum += min;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
