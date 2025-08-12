28c88134a8b25644e41cdea85aaf319e277ad9d8
AjmalAslam <aslamajmal55@gmail.com>
2025-08-13 02:57:50 +0800

    COMPLETED

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    const divideByFour = year % 4 === 0;
+    const divideBy400 = year % 400 === 0;
+    const divideBy100 = year % 100 === 0;
+
+    if(divideByFour && (divideBy400 || !divideBy100)){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
