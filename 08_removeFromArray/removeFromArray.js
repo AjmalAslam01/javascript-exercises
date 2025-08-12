147de15e0b83c8f71324eb01734bc2a49a384d7b
AjmalAslam <aslamajmal55@gmail.com>
2025-08-13 00:48:12 +0800

    COMPLETED

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numToRemove) {
+
+    return arr.filter(num => !numToRemove.includes(num));
+    
 };
 
 // Do not edit below this line
